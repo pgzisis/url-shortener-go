cf419e4c13eaa085566c8c3eff0de5d0f4e9f1b5
pgzisis <zisispg@gmail.com>
2022-03-24 17:03:41 +0100

    Handle yaml files

--- main.go
+++ main.go
@@ -1,36 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 
 	"github.com/pgzisis/url-shortener/urlshort"
 )
 
 func main() {
+	yamlFileName := getYamlFileName()
+	yaml := readYaml(yamlFileName)
+
 	mux := defaultMux()
 
-	// Build the MapHandler using the mux as the fallback
 	pathsToUrls := map[string]string{
 		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
 		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
 	}
 	mapHandler := urlshort.MapHandler(pathsToUrls, mux)
 
-	// Build the YAMLHandler using the mapHandler as the
-	// fallback
-	yaml := `
-- path: /urlshort
-  url: https://github.com/gophercises/urlshort
-- path: /urlshort-final
-  url: https://github.com/gophercises/urlshort/tree/solution
-`
-	yamlHandler, err := urlshort.YAMLHandler([]byte(yaml), mapHandler)
+	yamlHandler, err := urlshort.YAMLHandler(yaml, mapHandler)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Starting the server on :8080")
 	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func getYamlFileName() *string {
+	yamlFileName := flag.String("yaml", "pathsToUrls.yaml", "name of the yaml file to read")
+	flag.Parse()
+
+	return yamlFileName
+}
+
+func readYaml(yamlFileName *string) []byte {
+	yaml, err := ioutil.ReadFile(*yamlFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	return yaml
 }
 
 func defaultMux() *http.ServeMux {
--- urlshort/handler.go
+++ urlshort/handler.go
@@ -6,40 +6,18 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-// MapHandler will return an http.HandlerFunc (which also
-// implements http.Handler) that will attempt to map any
-// paths (keys in the map) to their corresponding URL (values
-// that each key in the map points to, in string format).
-// If the path is not provided in the map, then the fallback
-// http.Handler will be called instead.
 func MapHandler(pathsToUrls map[string]string, fallback http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		path := r.URL.Path
 
-		if url, present := pathsToUrls[path]; present {
-			http.Redirect(w, r, url, 302)
+		if url, ok := pathsToUrls[path]; ok {
+			http.Redirect(w, r, url, http.StatusFound)
 		} else {
 			fallback.ServeHTTP(w, r)
 		}
 	}
 }
 
-// YAMLHandler will parse the provided YAML and then return
-// an http.HandlerFunc (which also implements http.Handler)
-// that will attempt to map any paths to their corresponding
-// URL. If the path is not provided in the YAML, then the
-// fallback http.Handler will be called instead.
-//
-// YAML is expected to be in the format:
-//
-//     - path: /some-path
-//       url: https://www.some-url.com/demo
-//
-// The only errors that can be returned all related to having
-// invalid YAML data.
-//
-// See MapHandler to create a similar http.HandlerFunc via
-// a mapping of paths to urls.
 func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 	var pathsToUrls []pathToUrl
 
@@ -52,8 +30,8 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 		path := r.URL.Path
 
 		for _, pathToUrl := range pathsToUrls {
-			if path == pathToUrl.path {
-				http.Redirect(w, r, pathToUrl.url, 302)
+			if path == pathToUrl.Path {
+				http.Redirect(w, r, pathToUrl.Url, http.StatusFound)
 			}
 		}
 
@@ -62,6 +40,6 @@ func YAMLHandler(yml []byte, fallback http.Handler) (http.HandlerFunc, error) {
 }
 
 type pathToUrl struct {
-	path string
-	url  string
+	Path string `yaml:"path"`
+	Url  string `yaml:"url"`
 }
